c27aaae8724e72196c5e44a0937199c1dc5c4599
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-09 22:36:16 +0530

    ADDED: analyseMood to return HAPPY if null encountered

--- lib/src/main/java/MoodAnalyser/MoodAnalyser.java
+++ lib/src/main/java/MoodAnalyser/MoodAnalyser.java
@@ -4,11 +4,28 @@
 package MoodAnalyser;
 
 public class MoodAnalyser {
+	String message;
+	public MoodAnalyser(String message) {
+		this.message=message;
+	}
+	
     public String analyseMood(String message) {
         if(message.contains("sad"))
         	return "SAD";
         else
         	return "HAPPY";
+                
+    }
+    
+    public String analyseMood() {
+    	try {
+    		if(message.contains("sad"))
+    			return "SAD";
+    		else
+    			return "HAPPY";
+    	}catch(NullPointerException e) {
+    		return "HAPPY";
+    	}
                 
     }
 }
