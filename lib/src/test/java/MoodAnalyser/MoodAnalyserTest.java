bb3184f733da1c4d98bd1926820db16714213ca8
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-09 22:40:56 +0530

    REFACTORED: initialize message via construtor

--- lib/src/test/java/MoodAnalyser/MoodAnalyserTest.java
+++ lib/src/test/java/MoodAnalyser/MoodAnalyserTest.java
@@ -11,22 +11,22 @@ import org.junit.Assert;
 
 public class MoodAnalyserTest {
     @Test public void analyseMood_SadMessage_SAD() throws Exception {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        MoodAnalyser moodAnalyser = new MoodAnalyser("This is a sad message");
         
-        String mood=moodAnalyser.analyseMood("This is a sad message");
+        String mood=moodAnalyser.analyseMood();
         Assert.assertThat(mood, CoreMatchers.is("SAD"));
     }
     @Test public void analyseMood_SadMessage_HAPPY() throws Exception {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        MoodAnalyser moodAnalyser = new MoodAnalyser("This is a happy message");
         
-        String mood=moodAnalyser.analyseMood("This is a happy message");
+        String mood=moodAnalyser.analyseMood();
         Assert.assertThat(mood, CoreMatchers.is("HAPPY"));
     }
     
     @Test public void analyseMood_givenNullMood_HAPPY() {
-    	MoodAnalyser moodAnalyser = new MoodAnalyser();
+    	MoodAnalyser moodAnalyser = new MoodAnalyser(null);
         
-        String mood=moodAnalyser.analyseMood(null);
+        String mood=moodAnalyser.analyseMood();
         Assert.assertEquals("HAPPY",mood);
     	
     }
